bbd88e03f6e8dbcc9ee2ffb49e5cce357fa25ff7
travisprol0 <travisjohnprol@gmail.com>
2020-09-23 10:45:21 -0400

    about to work on filters

---
+++ src/Components/Filter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+class Filter extends React.Component {
+    render() {
+        return <h5>Filter</h5>
+    }
+}
+
+
+export default Filter
--- src/Containers/ProductContainer.js
+++ src/Containers/ProductContainer.js
@@ -1,11 +1,17 @@
 import React from "react"
 import Product from "../Components/Product"
+import Filter from "../Components/Filter"
 
 const ProductContainer = (props) => {
   let products = props.products.map((product) => (
     <Product key={product.id} product={product} />
   ))
-  return products
+  return (
+    <div>
+      <Filter products={props.products} />
+      {products}
+    </div>
+    )
 }
 
 export default ProductContainer
